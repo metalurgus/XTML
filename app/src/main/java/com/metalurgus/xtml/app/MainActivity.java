3fbfc93633cb5315088fcd67775a77ae8e17ee60
Vladyslav Matviienko <vladyslav.matviienko@bekey.dk>
2023-05-11 11:47:56 +0300

    Updated all dependencies to the latest versions

--- app/src/main/java/com/metalurgus/xtml/app/MainActivity.java
+++ app/src/main/java/com/metalurgus/xtml/app/MainActivity.java
@@ -1,7 +1,8 @@
 package com.metalurgus.xtml.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.metalurgus.xtml.XTML;
 
